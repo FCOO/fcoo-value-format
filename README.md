31885f82dd489f1e06bdda7add4d40845bb5f950
Niels Holt <niels@steenbuchholt.dk>
2017-01-25 15:23:00 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,122 @@
 # fcoo-value-format
-FCOO standard display formats when using jquery-value-format
+[jquery-value-format]:https://github.com/FCOO/jquery-value-format
+[fcoo-moment]:https://github.com/FCOO/fcoo-moment
+[moment-simple-format]:https://github.com/FCOO/moment-simple-format
+[fcoo-language]: https://github.com/FCOO/fcoo-language
+[i18n]:http://i18next.com/
+[fcoo-number]:https://github.com/FCOO/fcoo-number
+[numeral.js]:http://numeraljs.com/
+[fcoo-latlng-format]:https://github.com/FCOO/fcoo-latlng-format
+[latlng-format]:https://github.com/FCOO/latlng-format
+[fcoo-global-events]:https://github.com/FCOO/fcoo-global-events
+
+## Description
+`fcoo-value-format` combines a number of packages that formats different type of data (*"Formator"*) with a number of FCOO-packages that sets up different formats for different type of data (*"Administrator"*)
+[jquery-value-format] is used to define a number of standard formats to be used in FCOOs Web Applications
+
+### "Formator"
+in [FCOO Web Applications](https://github.com/FCOO/fcoo-application) we use the following packages to format and translate text, date, positions, and values
+
+- [i18n] (translation)
+- [moment-simple-format] (date and time format, timezone etc.)
+- [latlng-format] (position)
+- [numeral.js] (numeral formats)
+
+### "Administrator"
+To set up the different formats, to provide interaction between the different *Formator* (e.g. date-format includes translation of name of months), to save the users selection, and to fire the appropriate events defined in [fcoo-global-events] the following packages are created and used
+
+- [fcoo-language]
+- [fcoo-moment]
+- [fcoo-latlng-format]
+- [fcoo-number]
+
+### fcoo-value-format
+
+With [jquery-value-format] you assign a `value` and a (predefined) `format` to a html-element and [jquery-value-format] will format the value using the defined format
+If the user changes the format (e.g. date as `2017-01-30` instead of `30-01-2017`) `fcoo-value-format` would change all html-element containing date-values
+
+**`fcoo-value-format`** defines a number of different common formats to be used i FCOO Web Applications (see below)
+
+## Installation
+### bower
+`bower install https://github.com/FCOO/fcoo-value-format.git --save`
+
+## Demo
+http://FCOO.github.io/fcoo-value-format/demo/ 
+
+
+## Formats
+The following formats are defined. 
+If the `id` of the format ends with `_obj` the element keeps the pointer to the object providing the values and can be dynamic updated.
+Some formats have additional options. They are all optional.
+
+| id | input | Output ex. | Options (id/default/description) |
+| :--: | :--: | --- | --- |
+| `number`| `{number}` | `"1,000.123"` | `decimals` / `2` / Number of decimals. If the number is a integer no decimals are displayed<br>`format`, `"0,0[.]00"`, The [numeral.js] format used |
+| `distance`| `{number}` | `"123 nm"` | `unit` / `"m"`, `"m"`, `"km"`, or `"nm"` / The unit of the distance<br>`decimals`, `2`, See `number` |
+| `lat` | `{leaflet latLng}` | `"55°07'11.9"N"`  |  | 
+| `lng` | `{leaflet latLng}` | `"12°59'13.2"E"`  |  | 
+| `latlng` | `{leaflet latLng}` | `"55°07'11.9"N 13°07'22.7"E"` | `separator` / `" "` / Text or html-code between the lat and lng. E.g. `"<br>"` |
+| `time` | `moment` | `"14:00"` | |
+| `hour` | `moment` | `"14"` | |
+| `time_utc` | `moment` | `"13:00"` | |
+| `hour_utc` | `moment` | `"13"` | |
+| `date` | `moment` | `"25. Jan 2017"` | |
+| `date_long` | `moment` | `"Wednesday, 25. January 2017"` | |
+| `date_short` | `moment` | `"25/01/17"` | |
+| `date_utc` | `moment` | `"25. Jan 2017"` | |
+| `date_long_utc` | `moment` | `"Wednesday, 25. January 2017"` | |
+| `date_short_utc` | `moment` | `"25/01/17"` | |
+| `date_format_utc` | `moment` | `"25. Jan 2017"` | |
+| `datetime` | `moment` | `"25. Jan 2017 14:00"` | |
+| `datetime_long` | `moment` | `"Wednesday, 25. January 2017 14:00"` | |
+| `datetime_short` | `moment` | `"25/01/17 14:00"` | |
+| `datetime_utc` | `moment` | `"25. Jan 2017 13:00"` | |
+| `datetime_long_utc` | `moment` | `"Wednesday, 25. January 2017 13:00"` | |
+| `datetime_short_utc` | `moment` | `"25/01/17 13:00"` | |
+| `datetime_format_utc` | `moment` | `"25. Jan 2017 13:00"` | |
+| `timezone` | `moment` | `"Europe/Copenhagen"` | |
+| `timezone_full` | `moment` | `"Europe/Copenhagen (UTC+01:00)"` | |
+
+
+### Formats for objects
+If the "value" of the element is a object (`L.latLng` or `moment`) a copy of the object is saved and the original obk´ject can be altered without changing the contents of the element
+
+### Formats for `moment`
+- `_utc` : The displayed moment is in UTC
+- `_short`: The moment is displayed in a short format
+- `_long`: The moment is displayed in a long format
+
+Add the option `saveAsMoment: true` (default = `false`) to save the original moment-object instead of a copy. Allows for dynamic updates (see below)
+
+
+## Usage
+
+    //Return a div-element with the value 1000.123 formated to selected number-format (e.g. "1,000.123")
+    $('<div></div>').vfValueFormat( 1000.123, 'number' ); 
+    
+    //Return a div-element with the moment-object myMoment formated to selected date-format in current timezone (e.g. "30. jan 17")
+    $('<div></div>').vfValueFormat( myMoment, 'date' ); 
+
+    //Create a moment-obj with current time in selected format
+    var currentMoment = moment();
+    $('<div id="current-time"></div>')
+        .vfValueFormat( currentMoment, 'time', {saveAsMoment: true} ); 
+
+    window.setInterval(function(){ 
+        //Update currentMoment with current time...
+        $('#current-time").vfUpdate();    
+    }, 60*1000);
+
+
+
+
+
+## Copyright and License
+This plugin is licensed under the [MIT license](https://github.com/FCOO/fcoo-value-format/LICENSE).
+
+Copyright (c) 2017 [FCOO](https://github.com/FCOO)
+
+## Contact information
+
+Niels Holt nho@fcoo.dk
---
+++ src/fcoo-value-format.js
@@ -0,0 +1,230 @@
+/****************************************************************************
+	fcoo-value-format.js, 
+
+	(c) 2017, FCOO
+
+	https://github.com/FCOO/fcoo-value-format
+	https://github.com/FCOO
+
+****************************************************************************/
+
+(function ($, window, document, undefined) {
+	"use strict";
+	
+	//Create fcoo-namespace
+	window.fcoo = window.fcoo || {};
+	var ns = window.fcoo; 
+
+    /***************************************************************************
+    Updating formats on global events
+    languagechanged      : the language
+    datetimeformatchanged: the format of dates, time (13:00 or 01:00pm), the timezone, show/hide utc
+    numberformatchanged  : the format of numbers (1.000,123 or 1,000.123)
+    latlngformatchanged  : the format of posiitons/lat-lng
+    ***************************************************************************/
+    var updateGlobalEvent = '';
+    var formatIdList = [];
+    function setGlobalEvent( eventName ){
+        //Add event to current global-event
+        if (updateGlobalEvent && formatIdList.length){
+            var formats = formatIdList.join(' ');
+            ns.events.onLast( updateGlobalEvent, function(){ $.valueFormat.update( formats ); });
+        }
+        updateGlobalEvent = eventName;
+        formatIdList = [];
+    }
+
+    function addFormat( options ){
+        $.valueFormat.add( options );
+        formatIdList.push( options.id );          
+    }
+
+
+    /*************************************
+    NUMBER AND DISTANCE
+    *************************************/
+    setGlobalEvent( 'numberformatchanged' );
+
+    function getDecimals( options, defaultDecimals ){
+        return options.decimals !== undefined ? parseInt(options.decimals) : defaultDecimals !== undefined ? defaultDecimals : 2;
+    }
+
+    function round(value, decimals) {
+        return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+    }
+
+    function convertNumber( value ){
+        return $.isNumber( value ) ? value : parseFloat( value );
+    } 
+
+    function formatNumber( value, options ){
+        var decimals = getDecimals( options ),
+            format = options.format ? options.format : '0,0[.]' + (decimals ? Array(decimals+1).join('0') : '');
+        return window.numeral( value ).format ( format );
+    } 
+
+    /*************************************
+    formatId = number
+    *************************************/
+    addFormat({ 
+        id     : 'number', 
+        format : function( value, options ){ return formatNumber( value, options ); },
+        convert: convertNumber
+    });
+
+    /*************************************
+    formatId = distance
+    *************************************/
+    addFormat({ 
+        id     : 'distance', 
+        format : function( value, options ){ 
+            var factor = 1,
+                unitStr = ' m';
+            switch (options.unit){
+              case 'nm': factor = 1862; unitStr = ' nm';break;
+              case 'km': factor = 1000; unitStr = ' km';break;
+            }
+            
+            value = round( value/factor, getDecimals( options ) );
+            return formatNumber( value, options) + unitStr; 
+        },
+        convert: convertNumber
+    });
+    
+    /*************************************
+    LATLNG
+    *************************************/
+    setGlobalEvent( 'latlngformatchanged' );
+
+    function convertLatLng( latLng ){
+        return L.latLng( {lat:latLng.lat, lng:latLng.lng} );
+    }
+
+    /*************************************
+    formatId = lat, lng
+    *************************************/
+    addFormat({ id: 'lat', format : function( latLng ){ return latLng.formatLat(); }, convert: convertLatLng });
+    addFormat({ id: 'lng', format : function( latLng ){ return latLng.formatLng(); }, convert: convertLatLng });
+
+    /*************************************
+    formatId = latlng
+    *************************************/
+    addFormat({ 
+        id: 'latlng',
+        format : function( latLng, options ){ 
+            return latLng.format().join(options.separator ? options.separator : '&nbsp;');
+         }, 
+         convert: convertLatLng 
+     });    
+
+    
+    
+    /*************************************
+    DATE AND TIME (MOMENT)
+    *************************************/
+    setGlobalEvent( 'datetimeformatchanged' );
+
+    //Since changing language using moment.locale(...) do not change allready created moment-object 
+    //the moment are saved as a Date-object or as moment and 're-constructed' when the display is updated
+    function convertMoment( m, options ){ 
+        return options.saveAsMoment ? m : m.toDate();     
+    }
+    
+    function convertBackMoment( m, options ){ 
+        m = m ? m : moment();
+        return moment( options.saveAsMoment ? m.toDate() : m ); 
+    }
+
+    function addMomentFormat(id, formatFunc ){
+        addFormat({id: id, format: formatFunc, convert: convertMoment, convertBack: convertBackMoment });    
+    }
+
+
+    //Date-format named date_XX_obj 
+
+
+    /*************************************
+    formatId = time, hour
+    Time/hour in current timezone
+    *************************************/
+    addMomentFormat( 'time',     function( m ){ return m.tzMoment().timeFormat(); } );    
+    addMomentFormat( 'hour',     function( m ){ return m.tzMoment().hourFormat(); } );    
+    addMomentFormat( 'time_utc', function( m ){ return m.utc().timeFormat();      } );    
+    addMomentFormat( 'hour_utc', function( m ){ return m.utc().hourFormat();      } );    
+
+   
+    /*************************************
+    formatId = timezone, timezone_full
+    *************************************/
+    function timezoneFormat( options ){
+        var timezoneText = '',
+            timezoneFormat = options.showTimezone || 'NONE';
+        timezoneFormat = timezoneFormat.toUpperCase();
+        timezoneFormat = timezoneFormat == 'FULL'  ? 'F' :
+                         timezoneFormat == 'SHORT' ? 'S' : 
+                         timezoneFormat == 'NONE'  ? 'N' : 
+                         timezoneFormat;
+
+        switch (timezoneFormat){
+          case 'S': timezoneText = moment.simpleFormat.timezone.name; break;
+          case 'F': timezoneText = moment.simpleFormat.timezone.fullName; break;
+        }
+        
+        if (timezoneText && options.inclParenthesis)
+            timezoneText = '(' + timezoneText + ')';        
+        return timezoneText;
+    }   
+            
+    addFormat({ id: 'timezone',      format: function( dummy, options  ){ return  timezoneFormat( {showTimezone:'SHORT', inclParenthesis: options.inclParenthesis} );  } });    
+    addFormat({ id: 'timezone_full', format: function( dummy, options  ){ return  timezoneFormat( {showTimezone:'FULL',  inclParenthesis: options.inclParenthesis} );  } });    
+
+
+    /*************************************
+    formatId = date[_XX], datetime[_XX]
+    *************************************/
+    function momentFormat( m, options, dateFormat, datetime, utc ){
+        options = options || {};
+        if (dateFormat)
+            options.dateFormat = dateFormat;
+        var formatFunc = datetime ? moment.fn.dateTimeFormat : moment.fn.dateFormat,
+            timezoneText = timezoneFormat( options );
+        return formatFunc.call( utc ? m.utc() : m.tzMoment(), options ) + (timezoneText ? '&nbsp;'+timezoneText : '');    
+    }
+
+    //Format: 'FULL', 'SHORT', 'DIGITAL', 'NONE'
+    var dateDefaultFormat = { weekday: 'None',  month: 'Short',   year: 'Full' },
+        dateLongFormat    = { weekday: 'Full',  month: 'Full',    year: 'Full' },
+        dateShortFormat   = { weekday: 'None',  month: 'Digital', year: 'Short' };
+
+    addMomentFormat( 'date',                function( m, options ){ return momentFormat( m, options, dateDefaultFormat ); } );
+    addMomentFormat( 'date_long',           function( m, options ){ return momentFormat( m, options, dateLongFormat    ); } );
+    addMomentFormat( 'date_short',          function( m, options ){ return momentFormat( m, options, dateShortFormat   ); } );
+    addMomentFormat( 'date_format',         function( m, options ){ return momentFormat( m, options                    ); } );
+
+    addMomentFormat( 'date_utc',            function( m, options ){ return momentFormat( m, options, dateDefaultFormat, false, true ); } );
+    addMomentFormat( 'date_long_utc',       function( m, options ){ return momentFormat( m, options, dateLongFormat,    false, true ); } );
+    addMomentFormat( 'date_short_utc',      function( m, options ){ return momentFormat( m, options, dateShortFormat,   false, true ); } );
+    addMomentFormat( 'date_format_utc',     function( m, options ){ return momentFormat( m, options, null,              false, true ); } );
+    
+    addMomentFormat( 'datetime',            function( m, options ){ return momentFormat( m, options, dateDefaultFormat, true       ); } );
+    addMomentFormat( 'datetime_long',       function( m, options ){ return momentFormat( m, options, dateLongFormat,    true       ); } );
+    addMomentFormat( 'datetime_short',      function( m, options ){ return momentFormat( m, options, dateShortFormat,   true       ); } );
+    addMomentFormat( 'datetime_format',     function( m, options ){ return momentFormat( m, options, null,              true       ); } );
+
+    addMomentFormat( 'datetime_utc',        function( m, options ){ return momentFormat( m, options, dateDefaultFormat, true, true ); } );
+    addMomentFormat( 'datetime_long_utc',   function( m, options ){ return momentFormat( m, options, dateLongFormat,    true, true ); } );
+    addMomentFormat( 'datetime_short_utc',  function( m, options ){ return momentFormat( m, options, dateShortFormat,   true, true ); } );
+    addMomentFormat( 'datetime_format_utc', function( m, options ){ return momentFormat( m, options, null,              true, true ); } );
+    
+
+    //Flush global-events
+    setGlobalEvent( 'dummy' );
+
+/*
+    //Initialize/ready 
+	$(function() { 
+
+	
+	}); 
+*/
+}(jQuery, this, document));
